d98fba29aa9b426a21ef1125f23847251b62e1de
Ren <ren12tang@gmail.com>
2023-10-08 14:07:33 -0400

    wildcard test case

--- scrabble.py
+++ scrabble.py
@@ -6,11 +6,7 @@ def run_scrabble(rack):
     Returns a list of valid Scrabble words that can be constructed from the given rack, 
     sorted by their scores, and the total number of such valid words.
 
-    Args:
-    - rack (str): A string containing 2 to 7 characters representing the Scrabble rack.
-
-    Returns:
-    - tuple: A tuple containing two elements:
+    Returns a tuple containing two elements:
       1) A list of tuples, each containing the score and the word.
       2) An integer representing the total number of valid words.
     """
@@ -40,32 +36,35 @@ def run_scrabble(rack):
 
     return valid_scrabble_words, len(valid_scrabble_words)
 
-
 def _generate_words_from_rack(rack):
     """
-    A helper function that generates all possible words from the given rack, considering 3 cases of wildcards.
+    A helper function that generates all possible words from the given rack, considering wildcards.
     """
     rack = rack.upper()
     possible_words = set()
-    wildcards = ['*', '?']
-
-    if '*' in rack and '?' in rack:  # both wildcards present
+    
+    if rack.count('*') + rack.count('?') == 2:  # both wildcards present
         for letter1 in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
             for letter2 in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-                temp_rack = rack.replace('*', letter1, 1).replace('?', letter2, 1)
+                temp_rack = rack
+                if '*' in temp_rack:
+                    temp_rack = temp_rack.replace('*', letter1, 1)
+                if '?' in temp_rack:
+                    temp_rack = temp_rack.replace('?', letter2, 1)
                 for length in range(2, len(temp_rack) + 1):
                     for perm in permutations(temp_rack, length):
                         possible_words.add(''.join(perm))
-
-    elif any(wc in rack for wc in wildcards):  # only one wildcard present
+    elif '*' in rack or '?' in rack:  # only one wildcard present
         for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
             temp_rack = rack
-            for wc in wildcards:
-                temp_rack = temp_rack.replace(wc, letter, 1)
+            if '*' in temp_rack:
+                temp_rack = temp_rack.replace('*', letter, 1)
+            if '?' in temp_rack:
+                temp_rack = temp_rack.replace('?', letter, 1)
             for length in range(2, len(temp_rack) + 1):
                 for perm in permutations(temp_rack, length):
                     possible_words.add(''.join(perm))
-    else: # no wild card, no need to replace anything
+    else: # no wild card, so no need to replace anything
         for length in range(2, len(rack) + 1):
             for perm in permutations(rack, length):
                 possible_words.add(''.join(perm))
