54d490e9db48e4411f9860a49abc8578dd45c5ce
Ren <ren12tang@gmail.com>
2023-10-27 22:24:45 -0400

    improved according to pep8 standard

--- project1/auction_tang.py
+++ project1/auction_tang.py
@@ -1,11 +1,13 @@
 import random
 
-# from bidder_tang import Bidder
 
 class User:
+    """Represents a user in the auction system."""
+
     user_count = 0  # Class-level counter
 
     def __init__(self):
+        """Initialize the User instance with a random probability."""
         self.__probability = random.uniform(0, 1)
         self.user_id = User.user_count
         User.user_count += 1
@@ -17,19 +19,36 @@ class User:
         return f"User of id: {self.user_id}"
 
     def show_ad(self):
+        """Decide if an ad is shown to this user based on probability.
+
+        Returns:
+            bool: True if ad is shown, False otherwise.
+        """
         return random.random() < self.__probability
 
+
 class Auction:
+    """Manages an auction involving users and bidders."""
+
     def __init__(self, users, bidders):
+        """Initialize the Auction with given users and bidders.
+
+        Args:
+            users (list): List of User objects.
+            bidders (list): List of Bidder objects.
+        """
         self.users = users
         self.bidders = bidders
         self.balances = {bidder: 0 for bidder in bidders}
         self.user_ids = {user: i for i, user in enumerate(users)}
 
     def execute_round(self):
+        """Execute one round of the auction."""
         selected_user = random.choice(self.users)
         selected_user_id = self.user_ids[selected_user]
         bids = {}
+
+        # Collect bids from all bidders
         for bidder in self.bidders:
             bid_amount = bidder.bid(selected_user_id)
             bids[bidder] = bid_amount
@@ -37,22 +56,26 @@ class Auction:
         if len(self.bidders) < 2:
             return  # Not enough bidders to hold an auction
 
+        # Identify first and second place bidders and their bids
         sorted_bids = sorted(bids.items(), key=lambda x: x[1], reverse=True)
         first_place_bidder, first_place_bid = sorted_bids[0]
         second_place_bidder, second_place_bid = sorted_bids[1]
+
+        # Determine if the ad was clicked
         clicked = selected_user.show_ad()
-        
-        # Update the balances here
+
+        # Update balances
         if clicked:
             self.balances[first_place_bidder] += 1  # Reward for the click
         self.balances[first_place_bidder] -= second_place_bid  # Payment for the ad spot
-        
+
+        # Notify bidders
         for bidder in self.bidders:
-            is_winner = (bidder == first_place_bidder)
+            is_winner = bidder == first_place_bidder
             bidder.notify(is_winner, second_place_bid, clicked if is_winner else None)
 
 
-'''
+"""
 if __name__ == "__main__":
     # TODO: Remove before submission or place in a separate file (like main.py)
 
@@ -70,4 +93,4 @@ if __name__ == "__main__":
     for round_number in range(num_rounds):
         print(f"{round_number=}")
         auction.execute_round()
-'''
+"""
--- project1/bidder_tang.py
+++ project1/bidder_tang.py
@@ -1,21 +1,48 @@
 import random
 
+
 class Bidder:
+    """Represents a bidder in the auction system."""
+
     def __init__(self, num_users, num_rounds):
+        """Initialize the Bidder instance.
+
+        Args:
+            num_users (int): The number of users in the auction.
+            num_rounds (int): The number of auction rounds.
+        """
         self.num_users = num_users
         self.num_rounds = num_rounds
         self.__balance = 0
         self.history = {}
 
     def bid(self, user_id):
+        """Generate a random bid.
+
+        Args:
+            user_id (int): The user ID to bid for.
+
+        Returns:
+            float: The generated bid amount.
+        """
         bid_amount = random.uniform(0, 1)
         return round(bid_amount, 3)
 
     def notify(self, auction_winner, price, clicked):
+        """Update balance and history based on auction outcome.
+
+        Args:
+            auction_winner (bool): True if this bidder won, False otherwise.
+            price (float): The price paid in the auction.
+            clicked (bool or None): Whether the ad was clicked, if this bidder won.
+        """
         if auction_winner:
             self.__balance += 1 if clicked else 0  # Reward for the click
             self.__balance -= price  # Payment for the ad spot
 
-        # Existing logging code...
         unique_key = f"{random.randint(1, 1000)}_{random.randint(1, 1000)}"
-        self.history[unique_key] = {'won': auction_winner, 'price': price, 'clicked': clicked}
+        self.history[unique_key] = {
+            "won": auction_winner,
+            "price": price,
+            "clicked": clicked,
+        }
